023701f7c205f29dcfc5a84230ac7f57952bd048
KK\keisu <Keisuke061006@gmail.com>
2025-11-28 16:06:25 +0900

    Revert "リセット"
    This reverts commit ce4da3e3175fabf381c2f268d53f6d4dd59f81de.

--- slot777/slot777/Form1.Designer.cs
+++ slot777/slot777/Form1.Designer.cs
@@ -46,7 +46,6 @@
             this.pictureBox1.Size = new System.Drawing.Size(221, 113);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // pictureBox2
             // 
@@ -55,7 +54,6 @@
             this.pictureBox2.Size = new System.Drawing.Size(221, 113);
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
-            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
             // pictureBox3
             // 
@@ -68,7 +66,6 @@
             // 
             // button1
             // 
-            this.button1.Enabled = false;
             this.button1.Location = new System.Drawing.Point(342, 302);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(123, 96);
