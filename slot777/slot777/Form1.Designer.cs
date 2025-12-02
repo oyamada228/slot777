83014b58ad925e6f2c1dc482301cbdb6a5b42f52
KK\keisu <Keisuke061006@gmail.com>
2025-12-02 09:45:54 +0900

    不明

--- slot777/slot777/Form1.Designer.cs
+++ slot777/slot777/Form1.Designer.cs
@@ -66,6 +66,7 @@
             // 
             // button1
             // 
+            this.button1.Enabled = false;
             this.button1.Location = new System.Drawing.Point(342, 302);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(123, 96);
--- slot777/slot777/Form1.cs
+++ slot777/slot777/Form1.cs
@@ -32,17 +32,42 @@ namespace slot777
         {
             Image[] reelImages = {
           
-           
+              imo.png
+
+
             }
 
         }
+
+        Image[] numberImages = new Image[10];
+
+public void LoadImages()
+{
+    numberImages[0] = Properties.Resources.num0;
+    numberImages[1] = Properties.Resources.num1;
+    numberImages[2] = Properties.Resources.num2;
+    numberImages[3] = Properties.Resources.num3;
+    numberImages[4] = Properties.Resources.num4;
+    numberImages[5] = Properties.Resources.num5;
+    numberImages[6] = Properties.Resources.num6;
+    numberImages[7] = Properties.Resources.num7;
+    numberImages[8] = Properties.Resources.num8;
+    numberImages[9] = Properties.Resources.num9;
+}
+
+
+
+
+
+
+
 
         int index = 0;
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            index = (index + 1) % reelImages.Length;
-            pictureBox1.Image = reelImages[index];
+            index = (index + 1) % Image;
+            pictureBox1.Image = Image[index];
         }
 
         private void pictureBox3_Click(object sender, EventArgs e)
